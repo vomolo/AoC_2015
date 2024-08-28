586c467a32fcc346514591148659d8ac0ee8413b
vomolo <valentineomolo1.com>
2024-08-29 00:14:37 +0300

    FEAT: Added a go mod file. FUNC: Added a main file. FILE: Added a functions file. FUNC: Added a MakeSlice function to convert the strings into a slice of integers.

---
+++ day_3/functions/makeslice.go
@@ -0,0 +1,24 @@
+package functions
+
+func MakeSlice(str string) [][]int {
+	res := [][]int{}
+
+	for _, char := range str {
+		if char == '>' {
+			res = append(res, []int{1, 0})
+		}
+
+		if char == '<' {
+			res = append(res, []int{-1, 0})
+		}
+
+		if char == '^' {
+			res = append(res, []int{0, 1})
+		}
+
+		if char == 'v' {
+			res = append(res, []int{0, -1})
+		}
+	}
+	return res
+}
