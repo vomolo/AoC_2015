a3e2a52012950a970350075bb04a01a47cb87805
vomolo <valentineomolo1.com>
2024-09-01 18:48:54 +0300

    FUNC: Added main.go with a function that returns the lowest number that produces 5 zeros

---
+++ day_4/part1/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+)
+
+func findLowestNumber(secretKey string) int {
+	number := 1
+
+	for {
+
+		input := fmt.Sprintf("%s%d", secretKey, number)
+
+		hash := md5.Sum([]byte(input))
+		hashString := hex.EncodeToString(hash[:])
+
+		if hashString[:5] == "00000" {
+			return number
+		}
+		number++
+	}
+}
+
+func main() {
+	secretKey := "bgvyzdsv"
+	lowestNumber := findLowestNumber(secretKey)
+	fmt.Println(lowestNumber)
+}
