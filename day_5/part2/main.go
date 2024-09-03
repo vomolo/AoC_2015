685da39856c481d1097f011b61d0f5690ba71a84
vomolo <valentineomolo1@gmail.com>
2024-09-03 20:09:25 +0300

    FEAT: Added main.go to connect functions with the data.

---
+++ day_5/part2/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"part2/functions"
+)
+
+func main() {
+	useMes := "Expecting: go run your-program.go data.txt"
+
+	if len(os.Args) != 2 {
+		fmt.Println(useMes)
+		fmt.Println("Check length of arguments")
+		return
+	}
+	filePath := os.Args[1]
+
+	if !strings.HasSuffix(filePath, ".txt") {
+		fmt.Println(useMes)
+		fmt.Println("Make sure it's a .txt file extension")
+		os.Exit(1)
+	}
+	fileCheck, err := os.Stat(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("File does not exist")
+			return
+		}
+		fmt.Println("Error checking file:", err)
+		return
+	}
+	if fileCheck.Size() == 0 {
+		fmt.Println("File is empty")
+		return
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var arr []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		pair := functions.NonOverlappingPair(line)
+		fmt.Println(pair)
+
+		if pair != "" {
+			arr = append(arr, pair)
+		}
+
+	}
+
+	fmt.Println(len(arr))
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+}
